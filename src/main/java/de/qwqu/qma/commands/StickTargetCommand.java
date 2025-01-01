9ff94644380ebc22493b962049cd48a52714f31f
qwertyquarty <qwertyquarty@gmail.com>
2025-01-01 22:37:16 +0100

    stick: simplified target command

--- src/main/java/de/qwqu/qma/commands/StickTargetCommand.java
+++ src/main/java/de/qwqu/qma/commands/StickTargetCommand.java
@@ -1,15 +1,11 @@
 package de.qwqu.qma.commands;
 
-import java.util.UUID;
-
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import de.qwqu.qma.Addon;
+import de.qwqu.qma.commands.arguments.PlayerNameOrArgumentType;
 import meteordevelopment.meteorclient.commands.Command;
-import meteordevelopment.meteorclient.commands.arguments.PlayerListEntryArgumentType;
-import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.command.CommandSource;
-import net.minecraft.entity.Entity;
 
 public class StickTargetCommand extends Command {
   public StickTargetCommand() {
@@ -18,34 +14,9 @@ public class StickTargetCommand extends Command {
 
   @Override
   public void build(LiteralArgumentBuilder<CommandSource> builder) {
-    builder.then(argument("player", PlayerListEntryArgumentType.create()).executes(context -> {
-      PlayerListEntry lookUpTarget = PlayerListEntryArgumentType.get(context);
-      if (lookUpTarget == null) {
-        error("Couldn't find lookup target.");
-        return SINGLE_SUCCESS;
-      }
-
-      UUID uuid = lookUpTarget.getProfile().getId();
-      if (uuid == null) {
-        error("Player has no UUID.");
-        return SINGLE_SUCCESS;
-      }
-
-      Entity targetEntity = null;
-      for (Entity entity : mc.world.getEntities()) {
-        if (entity.getUuid().equals(uuid)) {
-          targetEntity = entity;
-          break;
-        }
-      }
-
-      if (targetEntity == null) {
-        error("Couldn't find player.");
-        return SINGLE_SUCCESS;
-      }
-
-      Addon.stick_targetEntity = targetEntity;
-      Addon.stick_targetName = targetEntity.getName().getString();
+    builder.then(argument("player", PlayerNameOrArgumentType.create()).executes(context -> {
+      // as simple as that, no need to overcomplicate it (see previous code)
+      Addon.stick_targetName = context.getInput().split(" ")[1];
 
       return SINGLE_SUCCESS;
     }));
---
+++ src/main/java/de/qwqu/qma/commands/arguments/PlayerNameOrArgumentType.java
@@ -0,0 +1,40 @@
+package de.qwqu.qma.commands.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.command.CommandSource;
+import java.util.concurrent.CompletableFuture;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
+public class PlayerNameOrArgumentType implements ArgumentType<String> {
+  private static final PlayerNameOrArgumentType INSTANCE = new PlayerNameOrArgumentType();
+
+  public static PlayerNameOrArgumentType create() {
+    return INSTANCE;
+  }
+
+  public static PlayerListEntry get(CommandContext<?> context) {
+    return context.getArgument("player", PlayerListEntry.class);
+  }
+
+  private PlayerNameOrArgumentType() {
+  }
+
+  @Override
+  public String parse(StringReader reader) throws CommandSyntaxException {
+    return reader.readString();
+  }
+
+  @Override
+  public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+    return CommandSource.suggestMatching(
+        mc.getNetworkHandler().getPlayerList().stream().map(playerListEntry -> playerListEntry.getProfile().getName()),
+        builder);
+  }
+}
