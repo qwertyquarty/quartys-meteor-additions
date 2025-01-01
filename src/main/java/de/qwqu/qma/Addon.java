bf52f837ba27ef0f452e44f24a6fba73dd0a28b8
qwertyquarty <qwertyquarty@gmail.com>
2025-01-01 22:00:22 +0100

    added stick target HUD element

--- src/main/java/de/qwqu/qma/Addon.java
+++ src/main/java/de/qwqu/qma/Addon.java
@@ -4,10 +4,13 @@ import com.mojang.logging.LogUtils;
 
 import de.qwqu.qma.commands.*;
 import de.qwqu.qma.modules.*;
+import de.qwqu.qma.hud.*;
 
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.entity.Entity;
@@ -17,6 +20,7 @@ import org.slf4j.Logger;
 public class Addon extends MeteorAddon {
   public static final Logger LOG = LogUtils.getLogger();
   public static final Category CATEGORY = new Category("QMA");
+  public static final HudGroup HUD_GROUP = new HudGroup("QMA");
 
   public static String stick_targetName = "";
   public static Entity stick_targetEntity = null;
@@ -33,6 +37,9 @@ public class Addon extends MeteorAddon {
     // Commands
     Commands.add(new EntityTPCommand());
     Commands.add(new StickTargetCommand());
+
+    // Hud
+    Hud.get().register(StickTarget.INFO);
   }
 
   @Override
---
+++ src/main/java/de/qwqu/qma/hud/StickTarget.java
@@ -0,0 +1,32 @@
+package de.qwqu.qma.hud;
+
+import de.qwqu.qma.Addon;
+import meteordevelopment.meteorclient.systems.hud.HudElement;
+import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
+import meteordevelopment.meteorclient.systems.hud.HudRenderer;
+import meteordevelopment.meteorclient.utils.render.color.Color;
+
+public class StickTarget extends HudElement {
+  public static final HudElementInfo<StickTarget> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "stick-target",
+      "Displays the stick target.", StickTarget::new);
+
+  public StickTarget() {
+    super(INFO);
+  }
+
+  @Override
+  public void render(HudRenderer renderer) {
+    if (isInEditor()) {
+      renderer.text("Target: qwertyquarty", x, y, Color.WHITE, true);
+      return;
+    }
+
+    if (Addon.stick_targetName == "")
+      return;
+
+    String displayText = "Target: " + Addon.stick_targetName;
+    setSize(renderer.textWidth(displayText, true), renderer.textHeight(true));
+    renderer.text(displayText, x, y, Color.WHITE, true);
+
+  }
+}
