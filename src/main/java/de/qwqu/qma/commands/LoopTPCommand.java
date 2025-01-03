cd010806037e4c249b191953b319325a13814fac
qwertyquarty <qwertyquarty@gmail.com>
2025-01-03 13:13:06 +0100

    used AntiCope's ClientPosArgumentType for looptp and orbit

--- src/main/java/de/qwqu/qma/commands/LoopTPCommand.java
+++ src/main/java/de/qwqu/qma/commands/LoopTPCommand.java
@@ -1,11 +1,11 @@
 package de.qwqu.qma.commands;
 
-import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.qwqu.qma.Addon;
 import meteordevelopment.meteorclient.commands.Command;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.command.CommandSource;
+import de.qwqu.qma.arguments.ClientPosArgumentType;
 
 public class LoopTPCommand extends Command {
   public LoopTPCommand() {
@@ -14,17 +14,16 @@ public class LoopTPCommand extends Command {
 
   @Override
   public void build(LiteralArgumentBuilder<CommandSource> builder) {
-    builder.then(argument("x", DoubleArgumentType.doubleArg())
-        .then(argument("y", DoubleArgumentType.doubleArg())
-            .then(argument("z", DoubleArgumentType.doubleArg())
-                .executes(context -> {
-                  if (!Modules.get().get("loop-teleport").isActive())
-                    Modules.get().get("loop-teleport").toggle();
+    builder.then(argument("pos", ClientPosArgumentType.pos()).executes(ctx -> {
+      if (!Modules.get().get("loop-teleport").isActive())
+        Modules.get().get("loop-teleport").toggle();
 
-                  Addon.ltp_x = context.getArgument("x", Double.class);
-                  Addon.ltp_y = context.getArgument("y", Double.class);
-                  Addon.ltp_z = context.getArgument("z", Double.class);
-                  return SINGLE_SUCCESS;
-                }))));
+      Addon.ltp_x = ClientPosArgumentType.getPos(ctx, "pos").x;
+      Addon.ltp_y = ClientPosArgumentType.getPos(ctx, "pos").y;
+      Addon.ltp_z = ClientPosArgumentType.getPos(ctx, "pos").z;
+
+      return SINGLE_SUCCESS;
+    }));
+
   }
 }
--- src/main/java/de/qwqu/qma/commands/OrbitCommand.java
+++ src/main/java/de/qwqu/qma/commands/OrbitCommand.java
@@ -1,8 +1,8 @@
 package de.qwqu.qma.commands;
 
-import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.qwqu.qma.Addon;
+import de.qwqu.qma.arguments.ClientPosArgumentType;
 import meteordevelopment.meteorclient.commands.Command;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.command.CommandSource;
@@ -14,20 +14,15 @@ public class OrbitCommand extends Command {
 
   @Override
   public void build(LiteralArgumentBuilder<CommandSource> builder) {
-    builder.then(argument("x", DoubleArgumentType.doubleArg())
-        .then(argument("y", DoubleArgumentType.doubleArg())
-            .then(argument("z", DoubleArgumentType.doubleArg())
-                .executes(context -> {
-                  if (!Modules.get().get("orbit").isActive())
-                    Modules.get().get("orbit").toggle();
+    builder.then(argument("pos", ClientPosArgumentType.pos()).executes(ctx -> {
+      if (!Modules.get().get("orbit").isActive())
+        Modules.get().get("orbit").toggle();
 
-                  Addon.orbit_x = context.getArgument("x", Double.class);
-                  Addon.orbit_y = context.getArgument("y", Double.class);
-                  Addon.orbit_z = context.getArgument("z", Double.class);
+      Addon.orbit_x = ClientPosArgumentType.getPos(ctx, "pos").x;
+      Addon.orbit_y = ClientPosArgumentType.getPos(ctx, "pos").y;
+      Addon.orbit_z = ClientPosArgumentType.getPos(ctx, "pos").z;
 
-                  return SINGLE_SUCCESS;
-                }))));
-
+      return SINGLE_SUCCESS;
+    }));
   }
-
 }
