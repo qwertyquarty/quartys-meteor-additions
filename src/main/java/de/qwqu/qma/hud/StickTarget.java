3116c093a255dd7d57b1090c562b9cbd7c0ad375
Bknibb <bailey.knibb@gmail.com>
2025-05-17 16:18:48 +1000

    Added textColor and alwaysRender options to StickTarget (#5)
    * Added textColor and alwaysRender options to StickTarget
    
    * Split the text colour into primaryColor and secondaryColor

--- src/main/java/de/qwqu/qma/hud/StickTarget.java
+++ src/main/java/de/qwqu/qma/hud/StickTarget.java
@@ -1,14 +1,37 @@
 package de.qwqu.qma.hud;
 
 import de.qwqu.qma.Addon;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.ColorSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
-import meteordevelopment.meteorclient.utils.render.color.Color;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 
 import java.util.Objects;
 
 public class StickTarget extends HudElement {
+  private final SettingGroup sgGeneral = settings.getDefaultGroup();
+  private final Setting<SettingColor> primaryColor = sgGeneral.add(new ColorSetting.Builder()
+    .name("primary-color")
+    .description("Primary Color.")
+    .defaultValue(new SettingColor())
+    .build()
+  );
+  private final Setting<SettingColor> secondaryColor = sgGeneral.add(new ColorSetting.Builder()
+    .name("secondary-color")
+    .description("Secondary Color.")
+    .defaultValue(new SettingColor(175, 175, 175))
+    .build()
+  );
+  private final Setting<Boolean> alwaysRender = sgGeneral.add(new BoolSetting.Builder()
+    .name("always-render")
+    .description("Always render the element even when there is no target.")
+    .defaultValue(false)
+    .build()
+  );
   public static final HudElementInfo<StickTarget> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "stick-target",
       "Displays the stick target.", StickTarget::new);
 
@@ -19,17 +42,18 @@ public class StickTarget extends HudElement {
   @Override
   public void render(HudRenderer renderer) {
     if (isInEditor()) {
-      renderer.text("Target: qwertyquarty", x, y, Color.WHITE, true);
+      double x = renderer.text("Target: ", this.x, y, primaryColor.get(), true);
+      renderer.text("qwertyquarty", x, y, secondaryColor.get(), true);
       setSize(renderer.textWidth("Target: qwertyquarty", true), renderer.textHeight(true));
       return;
     }
 
-    if (Objects.equals(Addon.stick_targetName, ""))
+    if (Objects.equals(Addon.stick_targetName, "") && !alwaysRender.get())
       return;
 
     String displayText = "Target: " + Addon.stick_targetName;
     setSize(renderer.textWidth(displayText, true), renderer.textHeight(true));
-    renderer.text(displayText, x, y, Color.WHITE, true);
-
+    double x = renderer.text("Target: ", this.x, y, primaryColor.get(), true);
+    renderer.text(Addon.stick_targetName, x, y, secondaryColor.get(), true);
   }
 }
