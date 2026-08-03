9a8fc8508da39d5dd982c87c279c3c6d567a8ae9
quarty <me@qwerrie.ee>
2026-08-03 23:10:09 +0200

    fix(NoPitchLimitMixin): remove pitch clamp in Entity.setPitch for Rotations.rotate too

--- src/main/java/de/qwqu/qma/mixin/NoPitchLimitMixin.java
+++ src/main/java/de/qwqu/qma/mixin/NoPitchLimitMixin.java
@@ -4,6 +4,7 @@ import de.qwqu.qma.modules.CameraUtils;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,6 +13,16 @@ import net.minecraft.entity.Entity;
 
 @Mixin(Entity.class)
 public abstract class NoPitchLimitMixin {
+  @Shadow
+  private float pitch;
+
+  @Inject(method = "setPitch", at = @At("HEAD"), cancellable = true)
+  private void onSetPitch(float pitch, CallbackInfo ci) {
+    if (!Modules.get().get(CameraUtils.class).noPitchLimit.get()) return;
+    this.pitch = pitch;
+    ci.cancel();
+  }
+
   @Inject(method = "changeLookDirection", at = @At("HEAD"), cancellable = true)
   private void removePitchClamp(double cursorDeltaX, double cursorDeltaY, CallbackInfo ci) {
     if (!Modules.get().get(CameraUtils.class).noPitchLimit.get()) return;
